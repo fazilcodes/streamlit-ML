31a400d2af1b1e219ca9bf22c89ca7d6f3e33793
Fazil <122675114+fazilcodes@users.noreply.github.com>
2024-10-29 22:16:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.title('⚔️ Fazicodes/ML-streamlit')
 st.info('Machine learning model')
 
 with st.expander('Data'):
-  st.write("Raw Data")
+  st.write('**Raw Data**')
   df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
   df
